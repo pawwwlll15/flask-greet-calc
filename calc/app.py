9936a4fbb836a853743ed6e7eff9d6c11f837208
Paul Mozeley <pdreier15@gmail.com>
2024-08-17 16:49:47 -0400

    completion of calc prject

--- calc/app.py
+++ calc/app.py
@@ -3,6 +3,43 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+def get_requests():
+    """return values of a and b requests"""
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return a,b
+
+@app.route('/add')
 def add():
-    return "add works"
+    """add a and b"""
+    a,b = get_requests()
+    return f"the total is: {a + b}"
+
+@app.route('/sub')
+def sub():
+    """subtract a and b"""
+
+    a,b = get_requests()
+    return f"the total is: {a - b}"
+
+@app.route('/mult')
+def mult():
+    """multiply a and b"""
+
+    a,b = get_requests()
+    return f"the total is: {a * b}"
+
+@app.route('/div')
+def div():
+    """divide a and b"""
+    a,b = get_requests()
+    return f"the total is: {a / b}"
+
+
+operators = {'add':add,'sub':sub,'mult':mult,'div':div}
+
+@app.route("/math/<oper>")
+def math_page(oper):
+    """ math operations on a and b"""
+    result = operators[oper]()
+    return result
